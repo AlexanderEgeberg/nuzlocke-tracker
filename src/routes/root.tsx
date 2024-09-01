c7d5c84d94271d42900532bd7a490f6ca79f3fbd
Alexander Egeberg <egebergalexander@gmail.com>
2024-09-01 13:45:33 +0200

    Add a simple start game landing page

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -2,8 +2,15 @@ import { Link } from "react-router-dom";
 
 export default function Root() {
   return (
-    <>
-      <Link to="/game">Game</Link>
-    </>
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="rounded-lg border-4 border-yellow-300 p-8 text-center">
+        <Link
+          to="/game"
+          className="text-lg font-bold text-yellow-300 hover:text-yellow-500"
+        >
+          Start Game
+        </Link>
+      </div>
+    </div>
   );
 }
